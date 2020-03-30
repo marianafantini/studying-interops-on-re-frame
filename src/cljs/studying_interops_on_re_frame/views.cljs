b641735a5b041a5c640a770c05fdcc6b84b90bc1
Mariana Fantini <mariana.fantini@gmail.com>
2020-03-30 13:42:18 -0300

    Added angular element to re-frame project

--- src/cljs/studying_interops_on_re_frame/views.cljs
+++ src/cljs/studying_interops_on_re_frame/views.cljs
@@ -1,11 +1,13 @@
 (ns studying-interops-on-re-frame.views
   (:require
    [re-frame.core :as re-frame]
-   [studying-interops-on-re-frame.subs :as subs]
-   ))
+   [studying-interops-on-re-frame.subs :as subs]))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (re-frame/subscribe [::subs/name])
+        to-do-list-data (.stringify js/JSON (clj->js [{:done true :text "import angular element on a re-frame project"}]))]
     [:div
-     [:h1 "Hello from " @name]
-     ]))
+     [:h1 "[interops] Hello from " @name]
+     [:app-hello-world {:id "app-hello-world-angular"
+                        :title "Click me!"
+                        :list to-do-list-data}]]))
